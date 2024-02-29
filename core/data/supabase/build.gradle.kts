c9a567f7c570d33261846df8c374b8d2b8b871de
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2024-02-29 16:27:39 +0300

    feature: supabase module implemented

---
+++ core/data/supabase/build.gradle.kts
@@ -0,0 +1,60 @@
+import dependencies.AppDependencies
+
+plugins {
+    id(AppPlugins.androidLibrary)
+    id(AppPlugins.androidKotlin)
+}
+
+android {
+    namespace = "ru.heatrk.languageapp.core.data.supabase"
+
+    compileSdk = AppConfig.Sdk.compile
+
+    defaultConfig {
+        minSdk = AppConfig.Sdk.min
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+
+            AppConfig.buildConfigFields(project).forEach { field ->
+                buildConfigField(field.type, field.name, "\"${field.releaseValue}\"")
+            }
+        }
+
+        debug {
+            AppConfig.buildConfigFields(project).forEach { field ->
+                buildConfigField(field.type, field.name, "\"${field.debugValue}\"")
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = AppConfig.jvmTarget
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    dependencies(
+        AppDependencies.Scout.core,
+    )
+
+    apiDependencies(
+        AppDependencies.Supabase.bom,
+        AppDependencies.Supabase.auth,
+        AppDependencies.Supabase.composeAuth,
+    )
+}
---
+++ core/data/supabase/src/main/java/ru/heatrk/languageapp/core/data/supabase/di/SupaBaseRegistry.kt
@@ -0,0 +1,23 @@
+package ru.heatrk.languageapp.core.data.supabase.di
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.gotrue.FlowType
+import ru.heatrk.languageapp.core.data.supabase.BuildConfig
+import scout.definition.Registry
+
+fun Registry.useSupaBaseBeans() {
+    singleton<SupabaseClient> {
+        createSupabaseClient(
+            supabaseUrl = BuildConfig.SUPABASE_URL,
+            supabaseKey = BuildConfig.SUPABASE_ANON_KEY,
+        ) {
+            install(Auth) {
+                flowType = FlowType.PKCE
+                scheme = "app"
+                host = "supabase.com"
+            }
+        }
+    }
+}
