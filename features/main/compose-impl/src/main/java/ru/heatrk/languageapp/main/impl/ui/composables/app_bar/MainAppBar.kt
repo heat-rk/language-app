6d4a2880659c48109110a67f37ac8acb3aabf467
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2024-03-30 13:17:08 +0300

    feature: main app bar animation improved

--- features/main/compose-impl/src/main/java/ru/heatrk/languageapp/main/impl/ui/composables/app_bar/MainAppBar.kt
+++ features/main/compose-impl/src/main/java/ru/heatrk/languageapp/main/impl/ui/composables/app_bar/MainAppBar.kt
@@ -4,24 +4,27 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -30,11 +33,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import coil.size.Size
 import ru.heatrk.languageapp.common.utils.PainterResource
 import ru.heatrk.languageapp.common.utils.Size
 import ru.heatrk.languageapp.common.utils.extract
 import ru.heatrk.languageapp.common.utils.painterRes
+import ru.heatrk.languageapp.core.design.composables.animation.FadeInAnimatedContent
 import ru.heatrk.languageapp.core.design.composables.shimmerEffect
 import ru.heatrk.languageapp.core.design.styles.AppTheme
 import ru.heatrk.languageapp.main.impl.R
@@ -160,34 +163,70 @@ private fun MainAppBarLoading(
 
 @Composable
 private fun MainAppBarLayout(
-    modifier: Modifier = Modifier,
     scrollingBehaviour: MainAppBarNestedScrollingConnection,
     avatarContent: @Composable () -> Unit,
     titleContent: @Composable () -> Unit,
     descriptionContent: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     val scrollProgress by scrollingBehaviour.getAppBarProgressState()
 
-    Column(
-        verticalArrangement = Arrangement.Bottom,
+    val appBarHeight by remember {
+        derivedStateOf {
+            MainAppBarCollapsedHeight + (MainAppBarHeight - MainAppBarCollapsedHeight) * scrollProgress
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.BottomStart,
         modifier = modifier
-            .layout { measurable, constraints ->
-                val placeable = measurable.measure(constraints)
-                layout(placeable.width, (placeable.height * scrollProgress).roundToInt()) {
-                    placeable.placeRelative(
-                        x = 0,
-                        y = (placeable.height * (scrollProgress - 1)).roundToInt()
-                    )
-                }
-            }
-            .alpha(scrollProgress)
             .fillMaxWidth()
-            .height(MainAppBarHeight)
+            .height(appBarHeight)
             .background(AppTheme.colors.primary)
             .padding(
                 horizontal = 24.dp,
                 vertical = 10.dp,
             )
+    ) {
+        FadeInAnimatedContent(
+            label = "AppBarHeightAnimation",
+            targetState = if (scrollProgress > 0.7) {
+                MainAppBarHeightState.Expanded
+            } else {
+                MainAppBarHeightState.Collapsed
+            }
+        ) { heightState ->
+            when (heightState) {
+                MainAppBarHeightState.Expanded -> {
+                    MainAppBarExpandedContent(
+                        avatarContent = avatarContent,
+                        titleContent = titleContent,
+                        descriptionContent = descriptionContent
+                    )
+                }
+                MainAppBarHeightState.Collapsed -> {
+                    MainAppBarCollapsedContent(
+                        avatarContent = avatarContent,
+                        titleContent = titleContent
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainAppBarExpandedContent(
+    avatarContent: @Composable () -> Unit,
+    titleContent: @Composable () -> Unit,
+    descriptionContent: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(AppTheme.colors.primary)
     ) {
         avatarContent()
 
@@ -201,6 +240,32 @@ private fun MainAppBarLayout(
     }
 }
 
+@Composable
+private fun MainAppBarCollapsedContent(
+    avatarContent: @Composable () -> Unit,
+    titleContent: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(AppTheme.colors.primary)
+    ) {
+        avatarContent()
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        titleContent()
+    }
+}
+
+private enum class MainAppBarHeightState {
+    Expanded,
+    Collapsed;
+}
+
 sealed interface MainAppBarState {
     data object Loading : MainAppBarState
 
@@ -210,6 +275,7 @@ sealed interface MainAppBarState {
     ) : MainAppBarState
 }
 
+val MainAppBarCollapsedHeight = 102.dp
 val MainAppBarHeight = 175.dp
 val AvatarSize = 54.dp
 
