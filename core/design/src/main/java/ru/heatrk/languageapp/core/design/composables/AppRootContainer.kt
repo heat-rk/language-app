512dcc0e304007195aee654a8aba3596fdae4111
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2024-04-03 21:18:06 +0300

    fix: app bar state not updating if quickly return to previous screen

--- core/design/src/main/java/ru/heatrk/languageapp/core/design/composables/AppRootContainer.kt
+++ core/design/src/main/java/ru/heatrk/languageapp/core/design/composables/AppRootContainer.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import ru.heatrk.languageapp.core.design.composables.scaffold.AppBarState
 import ru.heatrk.languageapp.core.design.composables.scaffold.AppScaffold
 import ru.heatrk.languageapp.core.design.composables.scaffold.LocalAppScaffoldController
 import ru.heatrk.languageapp.core.design.composables.scaffold.rememberAppScaffoldController
@@ -17,10 +18,13 @@ fun AppRootContainer(
         snackbarHostState = SnackbarHostState(),
     )
 
+    val currentAppBarState = appScaffoldController.appBarStates.lastOrNull()
+        ?: AppBarState.Hidden
+
     AppTheme { isDarkTheme ->
         AppScaffold(
             snackbarHostState = appScaffoldController.snackbarHostState,
-            appBarState = appScaffoldController.appBarState,
+            appBarState = currentAppBarState,
         ) {
             CompositionLocalProvider(LocalAppScaffoldController provides appScaffoldController) {
                 content(isDarkTheme)
--- core/design/src/main/java/ru/heatrk/languageapp/core/design/composables/scaffold/AppScaffoldController.kt
+++ core/design/src/main/java/ru/heatrk/languageapp/core/design/composables/scaffold/AppScaffoldController.kt
@@ -2,30 +2,25 @@ package ru.heatrk.languageapp.core.design.composables.scaffold
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 
 @Stable
 class AppScaffoldController(
     val snackbarHostState: SnackbarHostState,
-    initialAppBarState: AppBarState = AppBarState.Hidden,
 ) {
-    var appBarState by mutableStateOf<AppBarState>(initialAppBarState)
+    val appBarStates = mutableStateListOf<AppBarState>()
 }
 
 @Composable
 fun rememberAppScaffoldController(
     snackbarHostState: SnackbarHostState,
-    initialAppBarState: AppBarState = AppBarState.Hidden,
 ) = remember {
     AppScaffoldController(
         snackbarHostState = snackbarHostState,
-        initialAppBarState = initialAppBarState,
     )
 }
 
@@ -35,8 +30,13 @@ fun AppScaffoldControllerEffect(
 ) {
     val appScaffoldController = LocalAppScaffoldController.current
 
-    LaunchedEffect(appScaffoldController) {
-        appScaffoldController.appBarState = appBarState
+    DisposableEffect(appScaffoldController) {
+        appScaffoldController.appBarStates.add(appBarState)
+
+        onDispose {
+            appScaffoldController.appBarStates
+                .removeIf { it.key == appBarState.key }
+        }
     }
 }
 
