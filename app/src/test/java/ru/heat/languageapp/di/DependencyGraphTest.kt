527d80d601275a0760f47e97a1590022dc6f4e3f
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2024-04-12 16:34:20 +0300

    feature: dependencies graph validation test implemented

--- app/src/test/java/ru/heat/languageapp/di/DependencyGraphTest.kt
+++ app/src/test/java/ru/heat/languageapp/di/DependencyGraphTest.kt
@@ -1,10 +1,25 @@
+@file:OptIn(KoinExperimentalAPI::class)
+
 package ru.heat.languageapp.di
 
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
 import org.junit.Test
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.module
+import org.koin.test.verify.verify
+import ru.heatrk.languageapp.di.appModules
 
 class DependencyGraphTest {
     @Test
     fun `check dependency graph`() {
-        // TODO
+        module {
+            includes(appModules)
+        }.verify(
+            extraTypes = listOf(
+                HttpClientEngine::class,
+                HttpClientConfig::class,
+            )
+        )
     }
 }
